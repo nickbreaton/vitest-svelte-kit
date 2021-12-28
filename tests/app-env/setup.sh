a42fefa3836481e0eede753b31d9ed753ca182bd
Nick Breaton <nick@breaton.com>
2021-12-28 09:58:50 -0500

    Get test setup script working

--- tests/app-env/setup.sh
+++ tests/app-env/setup.sh
@@ -1,5 +1,4 @@
-# remove existing files
-git clean -Xf *
+#!/bin/bash
 
 # copy from template
 cp -rn ../../templates/svelte-kit-typescript/* .
@@ -7,5 +6,8 @@ cp -rn ../../templates/svelte-kit-typescript/* .
 # install packages
 pnpm add -D vite vitest vitest-svelte-kit@workspace:*
 
-# run tests
-pnpm vitest run
+# tweak package.json
+pnpm json -I -f package.json \
+    -e 'this.name="@vitest-svelte-kit/app-env"' \
+    -e 'this.private=true' \
+    -e 'this.scripts.test="vitest run"'
