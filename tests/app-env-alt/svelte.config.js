caa5878fac8ee1ca10e1775a8388a0fe270a72ef
Nick Breaton <nick@breaton.com>
2021-12-28 22:37:32 -0500

    Make sure svelte config is checked in

---
+++ tests/app-env-alt/svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		amp: true,
+
+		vite: {
+			test: {
+				environment: 'jsdom'
+			}
+		}
+	}
+};
+
+export default config;
